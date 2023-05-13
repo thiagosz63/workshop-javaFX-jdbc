1107906e8078496ae16a3c37964b79686c27199d
thiagosz63 <thiagosz63@gmail.com>
2023-05-13 00:26:32 -0300

    About view

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -8,7 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
+	private static Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -17,14 +18,20 @@ public class Main extends Application{
 			ScrollPane scrollPane = loader.load();
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollPane);
+
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-			} catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-			}
+		}
 	}
+
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
